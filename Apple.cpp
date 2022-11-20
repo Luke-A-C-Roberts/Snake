d4a35138b086214d4f9a324cf8ca05203092c754
Luke-A-C-Roberts <101272187+Luke-A-C-Roberts@users.noreply.github.com>
2022-11-20 21:37:38 +0000

    Update Apple.cpp

--- Apple.cpp
+++ Apple.cpp
@@ -1,8 +1,9 @@
 #include "Apple.h"
 
-/// \param width the width of the board
-/// \param height the height of the board
-/// \param snakePos the positions of the snake segments
+/// @brief Apple Initialiser
+/// @param width the width of the board
+/// @param height the height of the board
+/// @param snakePos the positions of the snake segments
 Apple::Apple(int width, int height, const std::vector<std::pair<int, int>>& snakePos) {
     std::srand(time(NULL));
 
@@ -21,13 +22,15 @@ Apple::Apple(int width, int height, const std::vector<std::pair<int, int>>& snak
     position = tempPos;
 }
 
+/// @brief returns the apple's position 
 std::pair<int, int> Apple::getPos() {
     return position;
 }
 
-/// \param width the width of the board
-/// \param height the height of the board
-/// \param snakePos the positions of the snake segments
+/// @brief Finds a new position for the apple
+/// @param width int, the width of the board
+/// @param height int, the height of the board
+/// @param snakePos const std::vector<std::pair<int,int>>& the positions of the snake segments
 void Apple::randomPos(int width, int height, const std::vector<std::pair<int, int>>& snakePos) {
     bool foundPos = false;
     std::pair<int, int> tempPos{};
@@ -46,6 +49,9 @@ void Apple::randomPos(int width, int height, const std::vector<std::pair<int, in
     position = tempPos;
 }
 
+/// @brief returns true if snakeFront perameter and Apple position are the same
+/// @param snakeFront std::pair<int, int> front position of the snake
+/// @return bool
 bool Apple::isEaten(std::pair<int, int> snakeFront) {
     return position == snakeFront;
 }
